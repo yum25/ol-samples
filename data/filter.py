ed5316252819fd6ed7d4bad508d361974ded9320
yumarie <yumarie@umich.edu>
2024-05-21 17:42:30 -0400

    add all bordering municipalities of detroit

--- data/filter.py
+++ data/filter.py
@@ -1,7 +1,29 @@
 import json
 
 def borders_detroit(county):
-    borders = ["Detroit"]
+    borders = ["Detroit", 
+               "Redford Twp", 
+               "Southfield", 
+               "Oak Park", 
+               "Royal Oak Twp", 
+               "Ferndale", 
+               "Hazel Park", 
+               "Warren", 
+               "Eastpointe", 
+               "Harper Woods", 
+               "Grosse Pointe Woods",
+               "Grosse Pointe Farms", 
+               "Grosse Pointe", 
+               "Grosse Pointe Park", 
+               "Hamtramck", 
+               "Detroit River", 
+               "Highland Park", 
+               "River Rouge", 
+               "Ecorse", 
+               "Lincoln Park", 
+               "Melvindale", 
+               "Dearborn", 
+               "Dearborn Heights"]
 
     return county["properties"]["name"] in borders
 
