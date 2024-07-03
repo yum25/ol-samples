8bf32a40d31c72f107f701efbe24846f3bfe1dfb
yumarie <yumarie@umich.edu>
2024-07-03 17:00:30 -0400

    fully render map on admin side

--- src/lib/components/map.svelte
+++ src/lib/components/map.svelte
@@ -1,44 +1,79 @@
 <script lang="ts">
 	import CanvasMap from 'ol/Map.js';
 	import View from 'ol/View.js';
-	import { getCenter } from 'ol/extent';
+	import VectorSource from 'ol/source/Vector';
+	import VectorLayer from 'ol/layer/Vector';
 	import { VectorTile } from 'ol/layer';
+	import GeoJSON from 'ol/format/GeoJSON';
+
+	import { getCenter } from 'ol/extent';
+	import { useGeographic } from 'ol/proj';
+	import { Polygon } from 'ol/geom';
 	import { stylefunction } from 'ol-mapbox-style';
 
 	import { onMount } from 'svelte';
-	import { baseSource, getCountiesExtent } from '../utils';
 
-	import styles from '../styles.json';
+	import { baseSource, getCountiesExtent } from '$lib/utils';
+	import { baseStyle } from '$lib/styles';
+
+	import boundaries from '$lib/references/boundaries.json';
+	import coords from '$lib/references/coords.json';
+	import styles from '$lib/styles.json';
 
 	export let id: string;
 	export let placements;
+	console.log(placements);
+	console.log(coords);
 	let map: CanvasMap;
 
+	useGeographic();
+
+	const geojsonFormatter = new GeoJSON();
 	const extent = getCountiesExtent();
 	const center = getCenter(extent);
-
-	const view = new View({
-		center,
-		extent,
-		zoom: 11,
-		minZoom: 11,
-		maxZoom: 17
-	});
 
 	const baseLayer = new VectorTile({
 		declutter: true,
 		source: baseSource
 	});
 
+	const boundarySource = new VectorSource({
+		features: geojsonFormatter.readFeatures(boundaries)
+	});
+
+	const boundaryLayer = new VectorLayer({
+		source: boundarySource,
+		style: (feature) => baseStyle(feature)
+	});
+
+	const view = new View({
+		center,
+		extent,
+		zoom: 2,
+		minZoom: 0,
+		maxZoom: 17
+	});
+
 	onMount(() => {
 		map = new CanvasMap({
 			target: id,
-			layers: [baseLayer],
+			layers: [baseLayer, boundaryLayer],
 			controls: [],
 			view
 		});
 
 		stylefunction(baseLayer, styles, 'esri');
+
+		boundarySource.getFeatures().forEach((feature) => {
+			feature.set('default', feature.clone());
+			if (
+				Object.keys(placements).includes(feature.get('name')) &&
+				feature.get('name') !== 'Detroit'
+			) {
+				console.log(feature.get('name'));
+				feature.setGeometry(new Polygon(placements[feature.get('name')]));
+			}
+		});
 	});
 </script>
 
@@ -46,6 +81,7 @@
 
 <style>
 	.map-container {
+		width: 30rem;
 		height: 30rem;
 	}
 </style>
