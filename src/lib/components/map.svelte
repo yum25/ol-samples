d6a0f452d30c14e549dbcea5475c72566f678372
yumarie <yumarie@umich.edu>
2024-07-03 11:20:45 -0400

    add map component

---
+++ src/lib/components/map.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import CanvasMap from 'ol/Map.js';
+	import { onMount } from 'svelte';
+
+	export let id;
+	let map: CanvasMap;
+
+	onMount(() => {});
+</script>
+
+<div {id}></div>
