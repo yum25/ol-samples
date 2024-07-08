17c392582d7b3358f48df84c2e460aa13ad09836
yumarie <yumarie@umich.edu>
2024-07-08 03:49:10 -0400

    style city selector

--- src/lib/components/cityselect.svelte
+++ src/lib/components/cityselect.svelte
@@ -11,11 +11,43 @@
 	on:focus={() => (focused = true)}
 	on:blur={() => setTimeout(() => (focused = false), 100)}
 />
-{#each cities as city}
-	{#if value.length > 0 && city.toLowerCase().includes(value.toLowerCase()) && focused}
-		<button type="button" on:click={() => (value = city)}>{city}</button>
-	{/if}
-{/each}
+{#if value.length > 0 && focused}
+	<div class="dropdown">
+		{#each cities as city}
+			{#if city.toLowerCase().includes(value.toLowerCase()) && focused}
+				<button type="button" on:click={() => (value = city)}>{city}</button>
+			{/if}
+		{/each}
+	</div>
+{/if}
 
 <style>
+	input {
+		outline: 1px solid black;
+		border: none;
+
+		height: 2rem;
+		font-size: 1rem;
+		padding: 0.25rem 0.25rem 0.25rem 1rem;
+	}
+
+	.dropdown {
+		max-height: 15rem;
+		overflow-y: scroll;
+
+		display: flex;
+		flex-direction: column;
+
+		outline: 1px solid lightgray;
+		box-shadow: 1px 1px 5px lightgray;
+	}
+
+	button {
+		border: none;
+		border-bottom: 1px solid lightgray;
+		background: none;
+
+		padding: 0.5rem;
+		font-size: 1rem;
+	}
 </style>
