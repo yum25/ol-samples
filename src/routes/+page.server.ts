853eaecb0aa57d640158e1a6aec298d47ca1596a
yumarie <yumarie@umich.edu>
2024-07-28 04:55:17 -0400

    adjust submission method to align with database schema

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,11 +1,12 @@
 import { sql } from '$lib/db.js';
 import { fail } from '@sveltejs/kit';
+import type { Geometry } from 'ol/geom.js';
 
 export const actions = {
 	default: async ({ request }) => {
 		const data = await request.formData();
 
-		const placements = data.get('features');
+		const placements = JSON.parse(data.get('features') as string) as Record<string, Geometry>;
 		const city_live = data.get('city_live');
 		const city_work = data.get('city_work');
 		const city_visit = data.get('city_visit');
@@ -15,10 +16,14 @@ export const actions = {
 			return fail(400);
 		}
 
-		const insertion = await sql`
-		INSERT INTO submissions (city_live, city_work, city_visit, city_avoid, placements)
-		SELECT '${city_live}', '${city_work}', '${city_visit}', '${city_avoid}', '${placements}'`;
+		const { rid } = (
+			await sql`
+			INSERT INTO submissions (city_live, city_work, city_visit, city_avoid)
+			VALUES (${city_live}, ${city_work}, ${city_visit}, ${city_avoid}) RETURNING rid`
+		)[0];
 
-		console.log(insertion);
+		Object.entries(placements).forEach(async ([name, geometry]: [string, Geometry]) => {
+			await sql`INSERT INTO placements (rid, name, location) VALUES(${rid}, ${name}, ST_GeomFromGeoJSON(${geometry}))`;
+		});
 	}
 };
