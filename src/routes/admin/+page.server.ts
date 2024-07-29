e1e688c32ffb1e217fe5e32314d0d188389b51bf
yumarie <yumarie@umich.edu>
2024-07-28 22:31:12 -0400

    modify admin route to join all placements by rid

--- src/routes/admin/+page.server.ts
+++ src/routes/admin/+page.server.ts
@@ -4,9 +4,12 @@ import { fail, type Actions } from '@sveltejs/kit';
 import type { PageServerLoad } from '../$types';
 
 export const load: PageServerLoad = async ({ cookies }) => {
-	const submissions = await sql(
-		`SELECT rid, placements, city_live, city_work, city_visit, city_avoid, created_at FROM submissions`
-	);
+	const submissions = await sql(`
+		SELECT rid, submissions.city_live, submissions.city_work, submissions.city_visit, submissions.city_avoid, submissions.created_at, 
+		json_object_agg(placements.name, ST_AsGeoJSON(placements.location)) as placements
+		FROM submissions
+		LEFT JOIN placements USING (rid)
+		GROUP BY rid`);
 
 	return { submissions };
 };
